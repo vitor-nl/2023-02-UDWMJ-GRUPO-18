c5bf5e4f48571e5d4c0a9773417a19bb55fb45ed
oEduMarques <eduardomesser321@gmail.com>
2023-11-30 13:07:16 -0300

    refiz tudo e mais um pouco

---
+++ apps/atendente.py
@@ -0,0 +1,28 @@
+import pessoa
+
+class Atendente(pessoa.Pessoa):
+    
+    def __init__(self, cpf, nome, datanasc, genero, estado, cidade, email, telefone):
+        super().__init__(cpf, nome, datanasc, genero, estado, cidade, email, telefone)
+
+    def verificar_excluir_consulta(self, paciente):
+        if paciente.consulta_agendada:
+            print(f"Consulta agendada para o paciente {paciente.nome}:")
+            paciente.verificar_consulta()
+
+            opcao = input("Deseja excluir esta consulta? (S/N): ").upper()
+            if opcao == 'S':
+                paciente.consulta_agendada = False
+                paciente.dados_consulta = {}
+                print("Consulta excluída com sucesso.")
+            else:
+                print("Consulta mantida.")
+        else:
+            print("Paciente não possui consulta agendada.")
+
+    def listar_consultas_agendadas(self, pacientes):
+        for paciente in pacientes:
+            if paciente.consulta_agendada:
+                print(f"Consulta agendada para o paciente {paciente.nome}:")
+                paciente.verificar_consulta()
+                print()
---
+++ apps/pessoa.py
@@ -0,0 +1,16 @@
+class Pessoa():
+    def __init__(self, cpf, nome, datanasc, genero, endereco, email, telefone, senha):
+        self.cpf = cpf
+        self.nome = nome
+        self.datanasc = datanasc
+        self.genero = genero
+        self.endereco = endereco
+        self.email = email
+        self.telefone = telefone
+        self.senha = senha
+        
+    # def mudar_senha(self):
+    #     nova_senha = input("Digite a nova senha: ")
+    #     self.senha = nova_senha
+    #     print("Sua senha foi alterada com sucesso, nova senha: ", self.senha)
+        
